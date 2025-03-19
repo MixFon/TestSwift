5feae39ea0cdc3ec00f2619c81462be8624ae9ff
mixfon <mixfon@rambler.ru>
2025-03-19 21:17:57 +0300

    Приведение типов

--- TempProj/TempProj/main.swift
+++ TempProj/TempProj/main.swift
@@ -5,7 +5,22 @@
 //  Created by Михаил Фокин on 19.03.2025.
 //
 
-import Foundation
+// Приведение типом. Из одного типа данных получаем доугой
+let one: Double = 1.5
+let two: Int = 2
 
-print("Hello, World!")
+// Оператор сложения
+let sumOneAndTwo = Int(one) + two
+print(sumOneAndTwo)
 
+// Оператор вычитания
+let diff = two - Int(one)
+print(diff)
+
+// Оператор произведения
+let multi = two * Int(one)
+print(multi)
+
+// Оператор деления
+let div = two / Int(one)
+print(div)
